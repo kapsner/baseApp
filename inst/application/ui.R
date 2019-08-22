de7d5997ac29bce013d214d5e80fa44be2625acd
kapsner <lorenz.kapsner@web.de>
2019-08-22 12:51:20 +0200

    added affiliation

--- inst/application/ui.R
+++ inst/application/ui.R
@@ -5,45 +5,49 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-shiny::shinyUI(shinydashboard::dashboardPage(skin = "black",
+shiny::shinyUI(shiny::tagList(shinydashboard::dashboardPage(skin = "black",
 
-                                             # Application title
-                                             shinydashboard::dashboardHeader(title = "A shiny baseApp"),
+                                                            # Application title
+                                                            shinydashboard::dashboardHeader(title = "A shiny baseApp"),
 
-                                             shinydashboard::dashboardSidebar(
+                                                            shinydashboard::dashboardSidebar(
 
-                                                 # Include shinyjs in the UI Sidebar
-                                                 shinyjs::useShinyjs(),
+                                                              # Include shinyjs in the UI Sidebar
+                                                              shinyjs::useShinyjs(),
 
-                                                 #Sidebar Panel
-                                                 shinydashboard::sidebarMenu(id = "tabs",
-                                                                             shinydashboard::menuItem("Settings", tabName = "settings", icon = icon("file")),
-                                                                             shiny::tags$hr(),
-                                                                             shiny::actionButton("reset", "Reset")
-                                                 )),
+                                                              #Sidebar Panel
+                                                              shinydashboard::sidebarMenu(id = "tabs",
+                                                                                          shinydashboard::menuItem("Settings", tabName = "settings", icon = icon("file")),
+                                                                                          shiny::tags$hr(),
+                                                                                          shiny::actionButton("reset", "Reset")
+                                                              ),
+                                                              shiny::div(class = "sidebar-menu", style = "position:fixed; bottom:0; left:0; white-space: normal; text-align:left;
+                                                                              padding: 9.5px 9.5px 9.5px 9.5px; margin: 6px 10px 6px 10px; box-sizing:border-box; heigth: auto; width: 230px;",
+                                                                         shiny::HTML("\u00A9 Lorenz A. Kapsner</i>"))
+                                                            ),
 
-                                             shinydashboard::dashboardBody(
+                                                            shinydashboard::dashboardBody(
 
-                                                 # Include shinyjs in the UI Body
-                                                 shinyjs::useShinyjs(),
+                                                              # Include shinyjs in the UI Body
+                                                              shinyjs::useShinyjs(),
 
-                                                 # js reset function
-                                                 # https://stackoverflow.com/questions/25062422/restart-shiny-session
-                                                 shinyjs::extendShinyjs(script = "reset.js", functions = "reset"), # Add the js code to the page
+                                                              # js reset function
+                                                              # https://stackoverflow.com/questions/25062422/restart-shiny-session
+                                                              shinyjs::extendShinyjs(script = "reset.js", functions = "reset"), # Add the js code to the page
 
-                                                 shinydashboard::tabItems(
-                                                     shinydashboard::tabItem(tabName = "settings",
-                                                                             moduleSettingsUI("moduleSettings")
-                                                     )
-                                                 )
-                                             )
-))
+                                                              shinydashboard::tabItems(
+                                                                shinydashboard::tabItem(tabName = "settings",
+                                                                                        moduleSettingsUI("moduleSettings")
+                                                                )
+                                                              )
+                                                            )
+)))
